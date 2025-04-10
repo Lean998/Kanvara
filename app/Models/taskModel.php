79c2aa89c9a1c7bb662bf50b0cee77fe3d5d2883
Lean998 <leandroaguero83@gmail.com>
2025-04-10 12:19:25 -0300

    correcion TaskModel
    Se realizaron correcciones  sobre el modelo de las tareas

--- app/Models/taskModel.php
+++ app/Models/taskModel.php
@@ -7,7 +7,20 @@
     protected $primaryKey = 'task_id';
     protected $useAutoIncrement = true;
     protected $returnType = 'array';
-    protected $createdField = 'task_created';
+    protected $createdField = 'task_created_at';
+    protected $updatedField  = 'task_updated_at';
+    protected $deletedField  = 'task_deleted_at'; 
+
+    protected $validationRules = [
+      'task_title'          => 'required|min_length[5]',
+      'task_desc'    => 'required|min_length[5]',
+      'task_priority' => 'required|in_list[baja,normal,alta]',
+      'task_state' => 'permit_empty',
+      'task_expiry'    => 'required|valid_date',
+      'task_reminder'  => 'permit_empty|valid_date',
+      'task_archived' => 'permit_empty',
+      'task_color'          => 'required',
+    ];
     protected $allowedFields = [
       'task_title', 
       'task_desc',
