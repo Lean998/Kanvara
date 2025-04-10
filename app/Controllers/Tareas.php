b5839b6492525b33f9c929765eb1724c7d315261
Lean998 <leandroaguero83@gmail.com>
2025-04-10 12:23:10 -0300

    add logica postCrearTarea
    Se agrego la logica para el manejo de alta de nuevas tareas.

--- app/Controllers/Tareas.php
+++ app/Controllers/Tareas.php
@@ -15,29 +15,29 @@
       echo '<h2> Datos de la tarea </h2>';
     }
 
-    public function getNuevaTarea() {	
+    
+    public function getNuevaTarea(){
+      return view('tarea/newTask', ['titulo' => 'Nueva Tarea']);
+    }
+    
+
+    public function postCrearTarea() {	
     $validation = service('validation');
       $rules = [
         'taskTitle' => 'required|min_length[5]|max_length[50]',
         'taskDesc' => 'required|min_length[10]|max_length[50]',
         'taskPriority' => 'required',
-        'taskExpiry' => 'required|valid_date|after_date[' . date('Y-m-d') . ']',
+        'taskExpiry' => 'required|valid_date|future_date',
         'taskColor' => 'required',
+        'taskReminderDate' => 'permit_empty|valid_date|future_date|before_date[taskExpiry]',
       ]; 
-
-      if ($this->request->getPost('taskReminder')) {
-        $rules['taskReminderDate'] = [
-            'rules' => 'required|valid_date|future_datetime|before_datetime[taskExpiry]',
-        ];
-      }
 
       $validation->setRules($rules);
       
       if(!$validation->withRequest($this->request)->run()){
         return redirect()->back()->withInput()->with('errors', $validation->getErrors());
       }
-
-      $taskModel = new taskModel();
+      
       $data = [
         'task_title' => $this->request->getPost('taskTitle'),
         'task_desc' => $this->request->getPost('taskDesc'),
@@ -45,11 +45,20 @@
         'task_state'=> 'En curso',
         'task_expiry' => $this->request->getPost('taskExpiry'),
         'task_color' => $this->request->getPost('taskColor'),
-        'task_archived' => false,
-        'user_id' => session('user_id'),
-        'user_password' => password_hash($this->request->getPost('password'), PASSWORD_DEFAULT)
+        'task_archived' => 0,
+        'user_id' => session('user_id')
       ];
+
+      $reminder = $this->request->getPost('taskReminderDate');
+
+      if (!empty($reminder)) {
+        $data['task_reminder'] = $reminder;
+      }
+
+      $taskModel = new TaskModel();
       $taskModel->save($data);
+
+      return redirect()->to('/tareas')->with('success', 'Tarea creada correctamente.');
   }
 }
 ?>
