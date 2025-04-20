464b6f2d378476ec93efb33e03da9c186135c8e9
Lean998 <leandroaguero83@gmail.com>
2025-04-19 21:03:15 -0300

    Update collaborationModel.php
    Se agrego un método para obtener los colaboradores de una tarea

--- app/Models/collaborationModel.php
+++ app/Models/collaborationModel.php
@@ -7,8 +7,19 @@
     protected $useAutoIncrement = true;
     protected $returnType = 'array';
     protected $allowedFields = [
+      'collaboration_id',
       'user_id',
       'task_id',
     ];
+
+    protected $createdField = 'collaboration_created';
+    protected $useTimestamps = true;
+
+    public function getColaboradores($taskId){
+      return $this->select('users.user_id, users.user_name')
+        ->join('users', 'users.user_id = collaborations.user_id')
+        ->where('collaborations.task_id', $taskId)
+        ->findAll();
+    }
   }
 ?>
