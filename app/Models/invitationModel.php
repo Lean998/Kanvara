3e95d080da786219da62f30060d6438158fe1426
Lean998 <leandroaguero83@gmail.com>
2025-04-29 12:02:27 -0300

    Update invitationModel
    Fix a la estructura y metodos

--- app/Models/invitationModel.php
+++ app/Models/invitationModel.php
@@ -1,7 +1,7 @@
 <?php
   namespace App\Models;
   use CodeIgniter\Model;
-  class invitationModel extends Model {
+  class InvitationModel extends Model {
     protected $table = 'invitations';
     protected $primaryKey = 'invitation_id';
     protected $useAutoIncrement = true;
@@ -12,15 +12,42 @@
       'invitation_email',
       'invitation_code',
       'invitation_created_at',
+      'invitation_updated_at',
       'invitation_expires_at',
       'invitation_used',
     ];
     protected $createdField = 'invitation_created_at';
     protected $updatedField  = 'invitation_updated_at';
-    protected $deletedField = 'invitation_deleted_at';
     protected $expiresField = 'invitation_expires_at';
     protected $useTimestamps = true;
-    protected $useSoftDeletes = true;
+
+    public function isInvitationValid($invitation_code){
+      $consulta = $this->where('invitation_code', $invitation_code)
+        ->where('invitation_expires_at >', date('Y-m-d H:i:s'))
+        ->where('invitation_used', 0)
+        ->first();
+      log_message('debug', 'SQL Query: ' . $this->db->getLastQuery());
+      return $consulta;            
+    }
+    public function getInvitacion($taskId, $email){
+      return $this->where('task_id', $taskId)
+      ->where('invitation_email', $email)
+      ->where('invitation_expires_at >', date('Y-m-d H:i:s'))
+      ->where('invitation_used', 0)
+      ->first();
+    }
+
+    public function getInvitacionByCode($taskId, $invitationCode){
+      return $this->where('task_id', $taskId)
+      ->like('invitation_code', $invitationCode)
+      ->where('invitation_used', 0)
+      ->where('invitation_expires_at >', date('Y-m-d H:i:s'))
+      ->first();
+    }
+
+    public function existingCode($codigo){
+      return $this->where('invitation_code', $codigo)->first();
+    }
 
     
   }
