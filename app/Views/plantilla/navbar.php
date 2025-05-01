c8f899c33c3d56e9b79fb5f0157010d4054b83de
Lean998 <leandroaguero83@gmail.com>
2025-05-01 16:30:55 -0300

    Update navbar.php
    Cambio de navbar

--- app/Views/plantilla/navbar.php
+++ app/Views/plantilla/navbar.php
@@ -1,36 +1,68 @@
-<nav class="navbar navbar-expand-lg bg-light ">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
     </div>
-  </div>
 </nav>
+
+<div class="offcanvas offcanvas-start bg-dark text-light" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title text-light" id="sidebarMenuLabel">Menú</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link text-light <?= $opcion == '' ? 'active' : '' ?>" href="<?= base_url() ?>">Mis Tareas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light <?= $opcion == 'compartidas' ? 'active' : '' ?>" href="<?= base_url('tareas/tareas-compartidas') ?>">Compartidas conmigo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light <?= $opcion == 'archivadas' ? 'active' : '' ?>" href="<?= base_url('tareas/tareas-archivadas') ?>">Archivadas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light <?= $opcion == 'eliminadas' ? 'active' : '' ?>" href="<?= base_url('tareas/tareas-eliminadas') ?>">Eliminadas</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<style>
+      /* Ajustes para el sidebar y el contenido principal */
+@media (max-width: 991.98px) { /* Punto de ruptura lg */
+    .main-content {
+        margin-left: 0 !important;
+    }
+    .container-fluid {
+        margin-left: 0 !important;
+    }
+    .container {
+        padding: 0.5rem; /* Reducir padding en pantallas pequeñas */
+    }
+}
+
+/* Estilos del sidebar */
+.nav-link.active {
+    background-color: #495057;
+    border-radius: 5px;
+}
+.nav-link:hover {
+    background-color: #6c757d;
+    border-radius: 5px;
+}
+#sidebar.collapsed {
+    width: 60px;
+    overflow: hidden;
+}
+#sidebar.collapsed .sidebar-content {
+    display: none;
+}
+#sidebar.collapsed h4 {
+    display: none;
+}
+.main-content.collapsed {
+    margin-left: 60px !important;
+}
+</style>
