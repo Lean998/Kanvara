cc87ffee0a22cf82cf9805374113d86755c5d389
Lean998 <leandroaguero83@gmail.com>
2025-05-02 20:00:13 -0300

    Create footer.php

---
+++ app/Views/plantilla/footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-light py-4 d-flex">
+    <div class="col-12 col-md-6 mb-3 justify-content-center mb-md-0">
+        <p class="mb-0  text-center">&copy; 2025 Kanvara. Todos los derechos reservados.</p>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</footer>
