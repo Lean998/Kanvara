203f0e48cbe41b099606a26899ade57669c66aac
Lean998 <leandroaguero83@gmail.com>
2025-05-02 12:20:05 -0300

    Delete Validation.php

--- app/Language/en/Validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// override core en language system validation or define your own en language validation message
-return [
-    'future_date' => 'La fecha debe ser posterior a la fecha actual',
-    'before_date' => 'La fecha debe ser anterior a la fecha de vencimiento',
-];
