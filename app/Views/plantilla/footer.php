3e9b2fe4cab7c58b3a070ac5e5c5b425b14e920c
Lean998 <leandroaguero83@gmail.com>
2025-05-02 20:12:41 -0300

    Update footer.php

--- app/Views/plantilla/footer.php
+++ app/Views/plantilla/footer.php
@@ -2,5 +2,4 @@
     <div class="col-12 col-md-6 mb-3 justify-content-center mb-md-0">
         <p class="mb-0  text-center">&copy; 2025 Kanvara. Todos los derechos reservados.</p>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </footer>
