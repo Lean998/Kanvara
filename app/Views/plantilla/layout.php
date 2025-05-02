b6dcc87bc9bd7c9381c5b0a7aa772b3e109e98c4
Lean998 <leandroaguero83@gmail.com>
2025-05-02 20:08:24 -0300

    Update layout.php

--- app/Views/plantilla/layout.php
+++ app/Views/plantilla/layout.php
@@ -27,7 +27,9 @@
     <?= $this->include('plantilla/footer') ?>
 
     <!-- Bootstrap JS -->
-    <script src="<?= base_url('public/scripts/funcionesTarea.js') ?>"></script>
+    <script>
+        const BASE_URL = '<?= base_url() ?>';
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
