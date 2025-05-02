c293ffdd7e376da6f78bf31cfa4cee1ad790b369
Lean998 <leandroaguero83@gmail.com>
2025-05-02 13:33:15 -0300

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,7 +9,13 @@ class Home extends BaseController {
     public function getIndex() {
     helper('form');
     $task = new TaskModel();
-    $tasks = $task->obtenerTareas(session('user_id'));
+    $ordenar = $this->request->getGet('ordenar');
+      if(!empty($ordenar)){
+        $tasks = $task->obtenerTareas(session('user_id'), $ordenar);
+      } else {
+        $tasks = $task->obtenerTareas(session('user_id'));
+      }
+    
     
     if($task == null){
       return $this -> response -> setJSON (['success' => false, 'message' => 'Tareas no encontradas']);
