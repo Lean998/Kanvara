0bb75f0cf1b3d9f2108e8c628006d3caabc83c7a
Lean998 <leandroaguero83@gmail.com>
2025-05-04 15:17:29 -0300

    Update layout.php

--- app/Views/plantilla/layout.php
+++ app/Views/plantilla/layout.php
@@ -28,9 +28,16 @@
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
+    <script src="<?= base_url('public/scripts/globales.js') ?>"></script>
     <script>
         const BASE_URL = '<?= base_url() ?>';
+    </script>
+    <script>
+        <?php if (session('success')): ?>
+            mostrarMensaje('mensaje-success', <?= json_encode(session('success')) ?>, 'success');
+        <?php elseif(session('error')): ?>
+            mostrarMensaje('mensaje-success', <?= json_encode(session('error')) ?>, 'danger');
+        <?php endif ?>
     </script>
 </body>
 </html>
