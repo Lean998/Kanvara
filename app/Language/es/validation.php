af605b16f0fc0cc8b370ca75195161e4c8426268
Lean998 <leandroaguero83@gmail.com>
2025-05-08 00:15:16 -0300

    Create validation.php

---
+++ app/Language/es/validation.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'future_date' => 'La fecha debe ser posterior a la fecha actual',
+    'before_date' => 'La fecha debe ser anterior a la fecha de vencimiento',
+    'required'    => 'El campo {field} es obligatorio.',
+    'min_length'  => 'El campo {field} debe tener al menos {param} caracteres.',
+    'max_length'  => 'El campo {field} no puede exceder los {param} caracteres.',
+    'valid_email' => 'El campo {field} debe contener una dirección de correo electrónico válida.',
+    'is_unique'   => 'El {field} ingresado ya se encuentra registrado.',
+    'exist_user_email' => 'El correo ingresado no existe en el sistema.',
+    'matches' => 'El campo {field} debe coincidir con el campo {param}'
+];
