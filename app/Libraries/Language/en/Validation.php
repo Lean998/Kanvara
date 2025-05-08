a79ac0c5881eb7e9b10f95643ae95fe47a2ee135
Lean998 <leandroaguero83@gmail.com>
2025-05-08 00:15:14 -0300

    Update Validation.php

--- app/Libraries/Language/en/Validation.php
+++ app/Libraries/Language/en/Validation.php
@@ -2,6 +2,5 @@
 
 // override core en language system validation or define your own en language validation message
 return [
-    'future_date' => 'La fecha debe ser posterior a la fecha actual',
-    'before_date' => 'La fecha debe ser anterior a la fecha de vencimiento',
+
 ];
